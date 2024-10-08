e1be8639096ac5b25e00bc2a01ed6577209d1a0e
Kamal Giri <93605875+kgiri2000@users.noreply.github.com>
2024-10-08 11:53:47 -0600

    Update main.py

--- AI_Assistant_with_GPT-4o/main.py
+++ AI_Assistant_with_GPT-4o/main.py
@@ -9,3 +9,33 @@ import pandas as pd
 client = openai.OPENAI()
 
 #Upload the papers
+
+# Run this
+papers = pd.DataFrame({
+    "filename": [
+        "2405.10313v1.pdf",
+        "2401.03428v1.pdf",
+        "2401.09395v2.pdf",
+        "2401.13142v3.pdf",
+        "2403.02164v2.pdf",
+        "2403.12107v1.pdf",
+        "2404.10731v1.pdf",
+        "2312.11562v5.pdf",
+        "2311.02462v2.pdf",
+        "2310.15274v1.pdf"
+    ],
+    "title": [
+        "How Far Are We From AGI?",
+        "EXPLORING LARGE LANGUAGE MODEL BASED INTELLIGENT AGENTS: DEFINITIONS, METHODS, AND PROSPECTS",
+        "CAUGHT IN THE QUICKSAND OF REASONING, FAR FROM AGI SUMMIT: Evaluating LLMs’ Mathematical and Coding Competency through Ontology-guided Interventions",
+        "Unsocial Intelligence: an Investigation of the Assumptions of AGI Discourse",
+        "Cognition is All You Need The Next Layer of AI Above Large Language Models",
+        "Scenarios for the Transition to AGI",
+        "What is Meant by AGI? On the Definition of Artificial General Intelligence",
+        "A Survey of Reasoning with Foundation Models",
+        "Levels of AGI: Operationalizing Progress on the Path to AGI",
+        "Systematic AI Approach for AGI: Addressing Alignment, Energy, and AGI Grand Challenges"
+    ]
+})
+papers["filename"] = "papers/" + papers["filename"]
+papers
